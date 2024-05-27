f65562541b2a4dd768b5e96c9c4ee0d6f06b23f3
Heitor Silva - FBS <heitor.silva@fbsconstrutora.com.br>
2024-05-26 23:00:04 -0300

    Criação do escopo do projeto feita.

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,34 @@
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        
+        /**Requisitos:
+         * Conhecer a classe Scanner
+         * Exibir as mensagens ao usuário
+         * Obter pela classe scanner os valores digitados no terminal
+         * Exibir a mensagem final da conta criada 
+         */
+
+         double saldo = 25;
+         double valorSolicitado = 18;
+
+         if (saldo >= valorSolicitado){
+            saldo = saldo - valorSolicitado;
+         }else{
+            System.out.println("Saldo insuficiente.");
+         }
+
+         System.out.println("Saldo atual: " + saldo);
+
+         System.out.println("-----------------------------------------------");
+
+         saldo = 15;
+         valorSolicitado = 22;
+
+         if (saldo >= valorSolicitado){
+            saldo = saldo - valorSolicitado;
+         }else{
+            System.out.println("Saldo insuficiente.");
+         }
+
+         System.out.println("Saldo atual: " + saldo);
     }
 }
